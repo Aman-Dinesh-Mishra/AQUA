fca70aeca4a97a2b6e722ac6994a21f5c33f726f
Aman-Dinesh-Mishra <155220475+Aman-Dinesh-Mishra@users.noreply.github.com>
2025-02-18 11:27:19 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import '../Navbar/Navbar.css'
 import LOGO from '../../assets/LOGO.png';
 import {Outlet, Link} from 'react-router-dom';
+import Contact from '../Contact/Contact';
+import { Button } from 'react-scroll';
 
 
 const Navbar = () => {
@@ -11,13 +13,13 @@ const Navbar = () => {
     <div className="desktopMenu">
      <Link className='desktopMenuListItem'to='/'>Home</Link>
      <Link className='desktopMenuListItem'to='/dashboard'>Dashboard</Link>
-     <Link className='desktopMenuListItem'to='/profile'>User Feedback</Link>
+     <Link className='desktopMenuListItem'to='/profile'>Survey</Link>
      <Link className='desktopMenuListItem'to='/login'>Login</Link>
      
      <Outlet />
 
     </div>
-    <button className="desktopMenuBtn">Contact Us</button>
+    <Link className="desktopMenuBtn" to='/contact'>Contact Us</Link>
     </nav>
   );
 }
